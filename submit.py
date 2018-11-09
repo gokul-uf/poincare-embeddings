1bb9d3743495fe197565130386b945aad3809929
gokul <gokul.uf@gmail.com>
2018-11-09 15:02:51 +0100

    have different namses for burnin and legit embeddings"

--- submit.py
+++ submit.py
@@ -10,20 +10,21 @@ if __name__ == "__main__":
             for ne in num_epochs:
                 config = f"pc_{nd}_d_{nn}_neg_{ne}_epoch"
                 op_name = f"embeddings/{config}.csv"
+                bi_name = f"embeddings/bi_{config}.csv"
                 print(f"running {config}")
                 burnin_cmd = (
                     f"./build/poincare -graph wordnet/noun_closure.tsv "
-                    f"-output-vectors {op_name} -start-lr 0.005 "
+                    f"-output-vectors {bi_name} -start-lr 0.005 "
                     f"-end-lr 0.005 -dimension {nd} -epochs 50 "
                     f"-number-negatives {nn} -distribution-power 1 "
                     f"-threads 8")
                 full_cmd = (
                     f"./build/poincare -graph wordnet/noun_closure.tsv "
-                    f"-output-vectors {op_name} -input-vectors {op_name} "
-                    f"-start-lr 0.5 -end-lr 0.5  -dimension {nd} "
+                    f"-output-vectors {op_name} -input-vectors {bi_name} "
+                    f"-start-lr 1. -end-lr 1.  -dimension {nd} "
                     f"-epochs {ne-50} -number-negatives {nn} "
                     "-distribution-power 0 -threads 8")
                 cmd = " ".join([burnin_cmd, "&&", full_cmd])
-                bsub_cmd = (f"bsub -n 2 -W 120:00 -o bsub_log/{config}.OUT "
+                bsub_cmd = (f"bsub -n 4 -W 120:00 -o bsub_log/{config}.OUT "
                             f" -e bsub_log/{config}.ERR -J {config} '{cmd}'")
                 os.system(bsub_cmd)
