5d9059840b303dd8ee48ae75ef05346e0202bc24
gokul <gokul.uf@gmail.com>
2018-11-12 03:07:50 +0100

    custom set csv dir, allocate 4G of memory to handle bigger embeddings

--- run_eval.py
+++ run_eval.py
@@ -3,13 +3,15 @@ import os
 import sys
 
 if __name__ == "__main__":
-    files = glob("embeddings/*.csv")
+    csv_dir = "/cluster/scratch/sgokula/trained_pc_embeddings"
+    files = glob(f"{csv_dir}/*.csv")
+    print(len(files))
 
     for f in files:
         f = os.path.basename(f)
-        bsub_cmd = (f"bsub -n 1 -J {f} -W 8:00 -o eval_op/{f}.OUT "
+        bsub_cmd = (f"bsub -n 1 -J {f} -W 8:00 -o eval_op/{f}.OUT -R 'rusage[mem=4096]' "
                     f"-e eval_op/{f}.ERR './evaluate "
-                    f"--graph wordnet/noun_closure.tsv --vectors embeddings/{f} "
-                    "--include-map'")
+                    f"--graph wordnet/noun_closure.tsv --vectors {csv_dir}/{f} "
+                    "--sample-seed 2 --include-map'")
         print(f"running {f}")
         os.system(bsub_cmd)
